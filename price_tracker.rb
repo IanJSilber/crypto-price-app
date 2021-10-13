1984dec7c2998a96cd2a52a2117cd4e6c376c41b
IanJSilber <iansilber32@gmail.com>
2021-10-13 00:24:49 -0400

    adds ability to create a portfolio as well as check its total value

--- price_tracker.rb
+++ price_tracker.rb
@@ -1,5 +1,8 @@
 require 'http'
+require 'tty-prompt'
 system 'clear'
+request = HTTP.get("https://api2.binance.com/api/v3/ticker/24hr")
+request = request.parse(:json)
 
 watchlist = []
 ians_watchlist = ["luna", "sol", "avax", "dydx", "eth"]
@@ -21,21 +24,43 @@ while bool1
   end
 end
 
-request = HTTP.get("https://api2.binance.com/api/v3/ticker/24hr")
-request = request.parse(:json)
+prompt = TTY::Prompt.new
+choice = prompt.select("pick an option", ["Check price data", "Check portfolio value"])
+
+default_amount = [41.5, 10, 20, 31.63, 0.059]
+amount = []
+if choice == "Check portfolio value"
+  if user_input != 'default'
+    watchlist.each do |ticker|
+      puts "enter how many #{ticker} coins you have"
+      num = gets.chomp.to_i
+      amount << num
+    end
+  elsif user_input == 'default'
+    amount = default_amount
+  end
+end
 
 bool2 = true
 i = 0
 i2 = 0
+port_value = []
 while bool2
   bool2 = false
   while i2 < watchlist.length
     symbol = request[i]["symbol"]
     if symbol.include? (watchlist[i2] + "USDT") 
-      puts symbol
-      puts request[i]["lastPrice"]
-      puts request[i]["priceChange"]
-      puts request[i]["priceChangePercent"]
+      if choice == "Check price data"
+        puts symbol
+        puts request[i]["lastPrice"]
+        puts request[i]["priceChange"]
+        puts request[i]["priceChangePercent"]
+      elsif choice == "Check portfolio value"
+        puts symbol
+        puts "Your total amount of #{watchlist[i2]} is $#{(request[i]["lastPrice"].to_i * amount[i2])}"
+        port_value << (request[i]["lastPrice"].to_i * amount[i2])
+        puts "your total account value is $#{port_value.sum}" 
+      end
       i2 += 1
       i = 0
       bool2 = true
